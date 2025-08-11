0cd6a324efc4a34492209b11f29ac732b4cbd8fd
FannAngels <EstefaniaJA04@gmail.com>
2025-08-11 17:47:47 -0600

    Jigsaw Puzzle implementado
    Algunos arreglos realizados en clase

--- Assets/Scripts/Lighter.cs
+++ Assets/Scripts/Lighter.cs
@@ -3,20 +3,123 @@ using UnityEngine.Rendering.Universal;
 
 public class Lighter : MonoBehaviour
 {
-    public bool hasLighter = false;
-    public Light2D characterLight; // Drag your child light object in the inspector
+    public bool hasLighter { get; private set; } = false;
+    [SerializeField]public Light2D characterLight; // Drag your child light object in the inspector
+    [SerializeField] private string pickupTag = "Lighter";
 
-    private void Start()
+    private GameObject pickup = null;
+
+    private void Awake()
     {
-        characterLight.enabled = false;
+        if (characterLight != null) characterLight.enabled = false;
     }
-    private void OnTriggerStay2D(Collider2D other)
+
+    private void Update()
+    {
+        // If we're near a pickup and press E, pick it up
+        if (!hasLighter && pickup != null && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+    private void PickUp()
+    {
+        if (pickup == null) return;
+
+        hasLighter = true;
+        Destroy(pickup);         // remove the world pickup
+        pickup = null;
+
+        if (characterLight != null) characterLight.enabled = true;
+
+        // Optional: notify other systems, raise an event, play SFX, UI update, etc.
+    }
+
+
+    /*private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Lighter") && Input.GetKeyDown(KeyCode.E))
         {
+            Debug.Log("Agarraste el encendedor");
             hasLighter = true;
             Destroy(other.gameObject);
             characterLight.enabled = true;
         }
+    }*/
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(pickupTag))
+        {
+            pickup = other.gameObject;
+            // Optional: show UI "Press E to pick up"
+            // Debug.Log("Lighter in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(pickupTag) && other.gameObject == pickup)
+        {
+            pickup = null;
+            // Optional: hide UI
+            // Debug.Log("Lighter out of range");
+        }
     }
 }
+
+/*public class Lighter : MonoBehaviour
+{
+    public bool hasLighter { get; private set; } = false;
+    [SerializeField] private Light2D characterLight;      // assign in Inspector
+    [SerializeField] private string pickupTag = "Lighter"; // tag of the lighter pickup object
+
+    private GameObject nearbyPickup = null; // currently overlapped pickup
+
+    private void Awake()
+    {
+        if (characterLight != null) characterLight.enabled = false;
+    }
+
+    private void Update()
+    {
+        // If we're near a pickup and press E, pick it up
+        if (!hasLighter && nearbyPickup != null && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUp();
+        }
+    }
+
+    private void PickUp()
+    {
+        if (nearbyPickup == null) return;
+
+        hasLighter = true;
+        Destroy(nearbyPickup);         // remove the world pickup
+        nearbyPickup = null;
+
+        if (characterLight != null) characterLight.enabled = true;
+
+        // Optional: notify other systems, raise an event, play SFX, UI update, etc.
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(pickupTag))
+        {
+            nearbyPickup = other.gameObject;
+            // Optional: show UI "Press E to pick up"
+            // Debug.Log("Lighter in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(pickupTag) && other.gameObject == nearbyPickup)
+        {
+            nearbyPickup = null;
+            // Optional: hide UI
+            // Debug.Log("Lighter out of range");
+        }
+    }
+}*/
--- Assets/Scripts/StartPuzzle.cs
+++ Assets/Scripts/StartPuzzle.cs
@@ -1,22 +1,86 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.SceneManagement;
 
 public class StartPuzzle : MonoBehaviour
 {
-    public CharacterController Player;
+    private GameObject player;
 
     [Header("Interaction Settings")]
     [SerializeField] public float interactionDistance = 1.0f;
 
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartPuzzleMiniGame();
+        }
+
+    }
+        private void StartPuzzleMiniGame()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) > interactionDistance)
+        {
+            return;
+        }
+        else
+        {
+            SceneManager.LoadScene("Puzzle");
+        }
+
+    }
+
+    //sound.PlayOneShot(buttonClick);      
+}
+
+
+
+
+
+
+    /*public PlayerMovement controller;
+    public GameObject puzzle;
+
+    [Header("Interaction Settings")]
+    [SerializeField] public float interactionDistance = 1.0f;
+
+    private GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        controller = player.GetComponent<PlayerMovement>();
+        puzzle.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartPuzzleMiniGame();
+        }
     }
-}
+
+    private void StartPuzzleMiniGame()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) > interactionDistance)
+        {
+            return;
+        }
+        else
+        {
+            puzzle.SetActive(true);
+
+            controller.enabled = false;
+        }
+
+    }*/
+
