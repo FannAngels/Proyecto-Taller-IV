9d9477f1dbc813d875ff5a7dd5d88fcdb5392e94
FannAngels <EstefaniaJA04@gmail.com>
2025-08-18 19:16:45 -0600

    GameJolt Sign In integrado

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -1,3 +1,5 @@
+using GameJolt.API;
+using GameJolt.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +7,25 @@ public class StartGame : MonoBehaviour
 {
     //public AudioSource sound;
     //public AudioClip buttonClick;
+    public GameObject startButton;
+
+    public void Start()
+    {
+        var isSignedIn = GameJoltAPI.Instance.CurrentUser != null;
+        if (isSignedIn)
+        {
+            GameJoltAPI.Instance.CurrentUser.SignOut();
+        }
+        //GameJoltAPI.Instance.CurrentUser.SignOut();
+        //GameJoltUI.Instance.ShowSignIn();
+        GameJoltUI.Instance.ShowSignIn((bool signInSuccess) => { startButton.SetActive(true); });
+    }
+
+    public void CheckLogin()
+    {
+
+    }
+
 
 
     public void GoToLevelOne()
